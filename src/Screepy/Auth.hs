3a312672b059c168891f94ab71ee5dad034ebd4e
kototama <kototamo@gmail.com>
2014-11-09 18:05:51 +0100

    add error management for the getBearerToken fn

--- src/Screepy/Auth.hs
+++ src/Screepy/Auth.hs
@@ -1,20 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Screepy.Auth (createBearerTokenCredentials,
-                     getBearerToken
-                    ) where
+                     getBearerToken,
+                     BearerToken,
+                     getToken)
+       where
 
 import           Control.Lens
-import qualified Data.ByteString.Base64 as B64
+import           Data.Aeson             (Result (Success), fromJSON)
+import qualified Data.Aeson.Lens        as L
 import qualified Data.ByteString        as B
-import qualified Data.ByteString.Lazy   as BL
+import qualified Data.ByteString.Base64 as B64
 import qualified Data.ByteString.Char8  as C
-import qualified Network.HTTP.Base           as Http
+import qualified Network.HTTP.Base      as Http
 import           Network.Wreq
 import           Network.Wreq.Lens
+import Control.Exception as E
+import qualified Network.HTTP.Client as HC
 
 newtype BearerTokenCredentials =
   BearerTokenCredentials { getCredentials :: B.ByteString } deriving Show
+
+newtype BearerToken = BearerToken { getToken :: B.ByteString } deriving Show
 
 createBearerTokenCredentials :: B.ByteString -> B.ByteString -> BearerTokenCredentials
 createBearerTokenCredentials key secret =
@@ -23,19 +30,22 @@ createBearerTokenCredentials key secret =
    in
    BearerTokenCredentials $ B64.encode (B.concat [k, ":", s])
 
-getBearerToken :: String -> BearerTokenCredentials -> IO String
+getBearerToken :: String -> BearerTokenCredentials -> IO (Either B.ByteString BearerToken)
 getBearerToken url bearerTokenCredentials = do
   let creds = getCredentials bearerTokenCredentials
       authContent = B.concat ["Basic ", creds]
-      opts = defaults
-             & header "Authorization" .~ [authContent]
-             & header "Content-Type" .~ ["application/x-www-form-urlencoded;charset=UTF-8"]
-  putStr $ "creds ="
-  B.putStr creds
-  putStr $ show opts
-  putStr "\n\n"
-  r <- postWith opts url ["grant_type" := ("client_credentials" :: B.ByteString)]
-  case r ^? responseBody of
-    Just body -> B.putStr (BL.toStrict body)
-    _ -> B.putStr "no body"
-  return "win"
+      opts = defaults & header "Authorization" .~ [authContent]
+  a <- E.try (postWith opts url ["grant_type" := ("client_credentials" :: B.ByteString)])
+  case a of
+    Left (HC.StatusCodeException s _ _) -> return . Left $ msg
+      where msg = B.concat [s ^. statusMessage,
+                            " (error ",
+                            C.pack . show $ s ^. statusCode,
+                            ")"]
+    Left e -> return . Left . C.pack $ show e
+    Right r ->
+      case r ^? responseBody . L.key "access_token" of
+        Just tokenV -> case (fromJSON tokenV :: Result String) of
+          Success token -> return $ Right . BearerToken . C.pack $ token
+          _ -> return . Left $ "Invalid response content"
+        _ -> return . Left $ "Invalid response content"
--- src/Screepy/Main.hs
+++ src/Screepy/Main.hs
@@ -1,6 +1,7 @@
 module Screepy.Main (main) where
 
 import qualified Data.ByteString.Char8  as C
+import qualified Data.ByteString        as B
 import qualified Screepy.Auth         as Auth
 import           Screepy.Config       (Config (..), loadConfig)
 
@@ -11,4 +12,7 @@ main = do
       s = C.pack (authSecret config)
       creds = Auth.createBearerTokenCredentials k s
   token <- Auth.getBearerToken "https://api.twitter.com/oauth2/token" creds
-  putStr $ show token
+  case token of
+    Left err -> do
+      B.putStr err
+    Right val -> B.putStr $ Auth.getToken val
