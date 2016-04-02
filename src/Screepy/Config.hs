712f6512ac46bff8e61600e8cd98e842feace564
Kototama <kototamo@gmail.com>
2016-04-02 21:21:41 +0200

    Add dump directory variable to the configuration

--- src/Screepy/Config.hs
+++ src/Screepy/Config.hs
@@ -5,19 +5,22 @@ module Screepy.Config (loadConfig, Config(..)) where
 import Data.ConfigFile
 import Data.Either.Utils (forceEither)
 
-data Config = Config { authKey :: String,
-                       authSecret :: String,
-                       baseUrl :: String                       
+data Config = Config { authKey :: String
+                     , authSecret :: String
+                     , baseUrl :: String
+                     , dumpDirectory :: FilePath
                      } deriving Show
 
-loadConfig :: String -> IO Config 
+loadConfig :: String -> IO Config
 loadConfig pathname = do
   val <- readfile emptyCP pathname
   let cp = forceEither val
   let key = forceEither $ get cp "api" "key"
   let secret = forceEither $ get cp "api" "secret"
   let url = forceEither $ get cp "api" "baseurl"
-  return Config { authKey = key,
-                  authSecret = secret,
-                  baseUrl = url}
-  
+  let dumpDir = forceEither $ get cp "screepy" "dumpDirectory"
+  return Config { authKey = key
+                , authSecret = secret
+                , baseUrl = url
+                , dumpDirectory = dumpDir
+                }
